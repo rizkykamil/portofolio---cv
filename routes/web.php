fcd4e015b153a192a390f6bdcb8a7056bf440ef1
rizkykamil <rabbaniikamil@gmail.com>
2024-09-09 21:00:57 +0700

    fix home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingPage\HomeController;
+use App\Http\Controllers\LandingPage\AboutController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/about', [AboutController::class, 'index'])->name('about');
 });
