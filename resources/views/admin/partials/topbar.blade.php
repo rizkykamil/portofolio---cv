a9ff0996a9905482d38bf1af02994caf40ad3339
rizkykamil <rabbaniikamil@gmail.com>
2024-09-15 21:05:09 +0700

    update fix top dan side bar

--- resources/views/admin/partials/topbar.blade.php
+++ resources/views/admin/partials/topbar.blade.php
@@ -39,7 +39,7 @@
                             <!-- item-->
                             <a href="javascript:void(0);" class="dropdown-item notify-item text-muted link-primary active">
                                 <div class="notify-icon">
-                                    <img src="assets/images/users/user-12.jpg" class="img-fluid rounded-circle" alt="" />
+                                    <img src="{{asset("admin/assets/images/users/user-12.jpg")}}" class="img-fluid rounded-circle" alt="" />
                                 </div>
                                 <div class="d-flex align-items-center justify-content-between">
                                     <p class="notify-details">Carl Steadham</p>
@@ -53,7 +53,7 @@
                             <!-- item-->
                             <a href="javascript:void(0);" class="dropdown-item notify-item text-muted link-primary">
                                 <div class="notify-icon">
-                                    <img src="assets/images/users/user-2.jpg" class="img-fluid rounded-circle" alt="" />
+                                    <img src="{{asset("admin/assets/images/users/user-2.jpg")}}" class="img-fluid rounded-circle" alt="" />
                                 </div>
                                 <div class="notify-content">
                                     <div class="d-flex align-items-center justify-content-between">
@@ -81,7 +81,7 @@
                             <!-- item-->
                             <a href="javascript:void(0);" class="dropdown-item notify-item text-muted link-primary">
                                 <div class="notify-icon">
-                                    <img src="assets/images/users/user-3.jpg" class="img-fluid rounded-circle" alt="" /> 
+                                    <img src="{{asset("admin/assets/images/users/user-3.jpg")}}" class="img-fluid rounded-circle" alt="" /> 
                                 </div>
                                 <div class="notify-content">
                                     <div class="d-flex align-items-center justify-content-between">
@@ -98,7 +98,7 @@
                             <!-- item-->
                             <a href="javascript:void(0);" class="dropdown-item notify-item text-muted link-primary">
                                 <div class="notify-icon">
-                                    <img src="assets/images/users/user-8.jpg" class="img-fluid rounded-circle" alt="" />
+                                    <img src="{{asset("admin/assets/images/users/user-8.jpg")}}" class="img-fluid rounded-circle" alt="" />
                                 </div>
                                 <div class="d-flex align-items-center justify-content-between">
                                     <p class="notify-details">Violette Lasky</p>
@@ -112,7 +112,7 @@
                             <!-- item-->
                             <a href="javascript:void(0);" class="dropdown-item notify-item text-muted link-primary">
                                 <div class="notify-icon">
-                                    <img src="assets/images/users/user-5.jpg" class="img-fluid rounded-circle" alt="" />
+                                    <img src="{{asset("admin/assets/images/users/user-5.jpg")}}" class="img-fluid rounded-circle" alt="" />
                                 </div>
                                 <div class="d-flex align-items-center justify-content-between">
                                     <p class="notify-details">Ralph Edwards</p>
@@ -126,7 +126,7 @@
                             <!-- item-->
                             <a href="javascript:void(0);" class="dropdown-item notify-item text-muted link-primary">
                                 <div class="notify-icon">
-                                    <img src="assets/images/users/user-6.jpg" class="img-fluid rounded-circle" alt="" /> 
+                                    <img src="{{asset("admin/assets/images/users/user-6.jpg")}}" class="img-fluid rounded-circle" alt="" /> 
                                 </div>
                                 <div class="notify-content">
                                     <div class="d-flex align-items-center justify-content-between">
@@ -152,7 +152,7 @@
 
                 <li class="dropdown notification-list topbar-dropdown">
                     <a class="nav-link dropdown-toggle nav-user me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                        <img src="assets/images/users/user-11.jpg" alt="user-image" class="rounded-circle">
+                        <img src="{{asset("admin/assets/images/users/user-11.jpg")}}" alt="user-image" class="rounded-circle">
                         <span class="pro-user-name ms-1">
                             Christian <i class="mdi mdi-chevron-down"></i> 
                         </span>
