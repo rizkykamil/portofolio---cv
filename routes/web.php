6d1030ec7c3616215539a02493102357daece569
rizkykamil <rabbaniikamil@gmail.com>
2025-03-09 11:29:04 +0700

    Configure admin routes with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ use App\Http\Middleware\MaintenanceMiddleware;
     Route::get('/works', [WorkController::class, 'index'])->name('works');
 // });// Middleware hanya diterapkan di grup ini
 
-// Route::prefix('admin')->middleware(MaintenanceMiddleware::class)->group(function () {
-    Route::prefix('auth')->group(function () {
-        Route::get('/login', [LoginController::class, 'index'])->name('login');
-        Route::post('/login-post', [LoginController::class, 'authenticate'])->name('login.authenticate');
-    });
+Route::prefix('auth')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login-post', [LoginController::class, 'authenticate'])->name('login.authenticate');
+});
+Route::prefix('/admin')->middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::prefix('blog')->group(function () {
         Route::get('/', [AdminBlogController::class, 'index'])->name('admin.blog.index');
@@ -44,7 +44,7 @@ use App\Http\Middleware\MaintenanceMiddleware;
         Route::put('/update/{id}', [AdminWorkController::class, 'update'])->name('admin.work.update');
         Route::delete('/delete/{id}', [AdminWorkController::class, 'destroy'])->name('admin.work.destroy');
     });
-// }); // Middleware hanya diterapkan di grup ini
+});
 
 // Route maintenance juga tanpa middleware
 // Route::get('/comming-soon', function () {
