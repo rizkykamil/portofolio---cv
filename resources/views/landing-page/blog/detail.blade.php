4bfc4630225ef0a43779fe6bae413e6635499826
rizkykamil <rabbaniikamil@gmail.com>
2025-03-18 14:36:48 +0700

    Move article details text styles to CSS for better organization and maintainability

--- resources/views/landing-page/blog/detail.blade.php
+++ resources/views/landing-page/blog/detail.blade.php
@@ -1,22 +1,6 @@
 @extends('landing-page.partials.app')
 @section('title', 'Contact Me')
 @section('content')
-<style>
-    h1, h2, h3, h4, h5, h6 {
-        color: var(--dark-theme-white);
-    }
-    li {
-        color: var(--dark-theme-light-white);
-    }
-    p {
-        color: var(--dark-theme-light-white);
-    }
-
-    /* bold p */
-    strong {
-        color: var(--dark-theme-white);
-    }
-</style>
 <section class="content-box-area mt-4">
     <div class="container">
         <div class="row g-4">
